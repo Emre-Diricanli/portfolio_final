babcf77c2170812e90a81e54115e73816df21581
Emre Diricanli <130785795+Emre-Diricanli@users.noreply.github.com>
2025-02-19 12:58:29 -0500

    google analytics fix?

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,50 +1,36 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import {ThemeProvider} from "@/components/theme-provider";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
-  subsets: ["latin"],
+  subsets: ["latin"]
 });
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
-  subsets: ["latin"],
+  subsets: ["latin"]
 });
 
 export const metadata: Metadata = {
   title: "Emre Diricanli",
-  description: "Portfolio",
+  description: "Portfolio"
 };
 
 export default function RootLayout({
-  children,
+  children
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark" style={{ colorScheme: 'dark' }}>
-      <script async src="https://www.googletagmanager.com/gtag/js?id=G-W9G8NWNXWY"></script>
-        <script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'G-W9G8NWNXWY');
-        </script>
+    <html lang="en" className="dark" style={{ colorScheme: "dark" }}>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-      {/*<ThemeProvider*/}
-      {/*    attribute="class"*/}
-      {/*    defaultTheme="dark"*/}
-      {/*    // enableSystem*/}
-      {/*    // disableTransitionOnChange*/}
-      {/*> */}
-          {children}
-      {/*</ThemeProvider>*/}
+        <GoogleAnalytics gaId="G-W9G8NWNXWY" />
 
+        {children}
       </body>
     </html>
   );
