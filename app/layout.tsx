1cd51e28cdecfa82c70a3ffae7b91176b4e02e30
emrediricanli <diricanliemre@gmail.com>
2025-05-08 19:36:52 -0400

    Speed Insights
    Signed-off-by: emrediricanli <diricanliemre@gmail.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { GoogleAnalytics } from "@next/third-parties/google";
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,6 +32,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Analytics/>
+        <SpeedInsights/>
         <GoogleAnalytics gaId="G-W9G8NWNXWY" />
 
         {children}
